e4e9731a17686148bd4a7f640d91404b7f82157a
ethun <ethuntcg@gmail.com>
2024-11-05 00:18:54 +0600

    yooyoo

---
+++ Gsap.js
@@ -0,0 +1,22 @@
+gsap.from(".box",{
+          x:1200,
+          rotate:360,
+          duration:2,
+          borderRadius:50,
+          scale:0.5,
+          opacity:0,
+          repeat:2,
+          yoyo:true,
+          stagger:0.4,
+
+})
+
+// gsap.from("h1",{
+//           opacity:0,
+//           duration:0.6,
+//           repeat:2,
+//           yoyo:true,
+//           stagger:0.4,
+// })
+
+

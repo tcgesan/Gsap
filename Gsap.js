3674e51a90924551cd15fe86b7c964c4a2787ce7
ethun <ethuntcg@gmail.com>
2024-11-05 11:58:50 +0600

    upp

--- Gsap.js
+++ Gsap.js
@@ -1,15 +1,15 @@
-gsap.from(".box",{
-          x:200,
-          rotate:360,
-          duration:2,
-          borderRadius:50,
-          scale:0.5,
-          opacity:0,
-          repeat:-1,
-          yoyo:true,
-          stagger:0.4,
+// gsap.from(".box",{
+//           x:200,
+//           rotate:360,
+//           duration:2,
+//           borderRadius:50,
+//           scale:0.5,
+//           opacity:0,
+//           repeat:-1,
+//           yoyo:true,
+//           stagger:0.4,
 
-})
+// })
 
 // gsap.from("h1",{
 //           opacity:0,
@@ -20,3 +20,40 @@ gsap.from(".box",{
 // })
 
 
+// gsap.to(".nav",{
+//           x:880,
+//           y:30,
+//           duration:0.4,
+// })
+
+
+gsap.from(".page1 .box",{
+          // x:500,
+          rotate:360,
+          scale:0,
+          duration:1
+})
+
+gsap.from(".page2 .box",{
+          // x:500,
+          rotate:360,
+          scale:0,
+          duration:1,
+          scrollTrigger:{
+                    trigger:".page2 .box",
+                    scrub:2,
+                    markers:true,
+                    scroller:"body",
+          }
+})
+
+gsap.from(".page3 .box",{
+          x:-300,
+          scale:0,
+          duration:1,
+          scrollTrigger:{
+                    trigger:".page3 .box",
+                    scrub:2,
+                    scroller:"body",
+          }
+})
